051eeb711b20ce364c64880d9bd4550fc6ea8aec
jfh <jhfj>
2026-02-16 11:53:30 +0300

    Изменяю шим и микросхему

---
+++ get-dac/mcpdriver4725_driver.py
@@ -0,0 +1,36 @@
+import smbus 
+class MCP4725:
+    def __init__(self,dynamic_range,address = 0x61, verbose = True):
+        self.bus = smbus.SMbus(1)
+        self.adress = address
+        self.wm = 0x00
+        self.pds = 0x00
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
+    def deinit(self):
+        self.bus.close()
+    def set_number(self,number):
+        if not isinstance (number,int):
+            print(" На вход ЦАП можно подавать только целые числа")
+        if not (0 <= number<= 4095):
+            print("Число выходит за разрядность драйвера 12 бит")
+        first_byte =self.wm | self.pds | number >>8 
+        second_byte= number & 0xFF
+        self.bus.write_byte_data(0x61,first_byte, second_byte)
+
+        if self.verbose:
+            print(f"Число: {number},отправленные по I2c данные [0x{(self.adress<<1):02X},0x{first_byte:02X}, 0x{second_byte:02X}\n")
+    def set_voltage(self, voltage):
+        max_number = 2**len(self.gpio_bits) - 1
+        number = int(voltage * max_number / self.dynamic_range)
+        number = max(0, min(number, max_number))
+        self.set_number(number)
+if __name__=="__main__":
+    dac=MCP4725(dynamic_range = 3.3,address=0x61,verbose=True)
+    try:
+        dec.set_voltage(3.3)
+    finally:
+        dac.deinit()
+
+
+
