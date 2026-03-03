33448c7e9d3306a1db44fe1fd88faf87facb4329
Алексей Давыдов <davydov.aa@phystech.edu>
2026-03-03 11:53:06 +0300

    График1

---
+++ git -adc/r2r-sar.py
@@ -0,0 +1,21 @@
+import adc_plot as plot
+import R2R_ADC_module as adc
+import time
+
+adc_object = adc.R2R_ADC(dynamic_range=3.3)
+
+voltages = []
+times = []
+duration = 5.0
+
+try:
+    start = time.time()
+    while (time.time() - start) < duration:
+        voltage = adc_object.get_sar_voltage()
+        voltages.append(voltage)
+        times.append(time.time() - start)
+    
+    plot.plot_voltage_vs_time(times, voltages, 3.3)
+
+finally:
+    adc_object.clean()
